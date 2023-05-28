5e00ceaf9292b93f83185c9dc7725ec1444ab596
Eduardo-Loiola <128156137+Eduardo-Loiola@users.noreply.github.com>
2023-05-28 16:46:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-a = int(input("Digite um número para um lado: "))
-b = int(input("Digite um segundo número para o segundo lado: "))
-c = int(input("Digite um terçeiro número para o terçeiro lado : "))
-if (a + b) < c or (b + c) < a or (a + c) < b: 
-    print("Não é um triângulo.")
-elif a == b and b == c:
-    print("Equilátero.")
-elif a == b or b == c or a == c:
-    print("Isósceles.")
+a = int(input("Digite o valor do primeiro lado: "))
+b = int(input("Digite o valor do segundo lado: "))
+c = int(input("Digite o valor do terceiro lado: "))
+if a + b > c and b + c > a and a + c > b:
+    if a == b == c:
+        tipo_triangulo = "Equilátero"
+    elif a == b or b == c or a == c:
+        tipo_triangulo = "Isósceles"
+    else:
+        tipo_triangulo = "Escaleno"
 else:
-    print("Escaleno.")
+    tipo_triangulo = "Não é um triângulo"
+print("O tipo de triângulo formado é:", tipo_triangulo)
